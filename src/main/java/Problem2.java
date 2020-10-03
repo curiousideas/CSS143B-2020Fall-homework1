fb9949eba1dfad10a0ecf9f9151ed0d35313a823
Dani <shaykhodani@gmail.com>
2020-10-02 17:24:32 -0700

    Fifth commit (:

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -2,6 +2,33 @@ public class Problem2 {
 
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
-        // Add your solution
+        // From smallest to largest
+        boolean sorted = false;
+        int temp;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i] > data[i + 1]) {
+                    temp = data[i];
+                    data[i] = data[i + 1];
+                    data[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+         // I copied the code from this website: https://stackabuse.com/bubble-sort-in-java/
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i]);
+        }
+        System.out.println();
+            /*if (i == 0) {
+                System.out.print("{" + data[i]);
+            } else {
+                System.out.print(", " + data[i]);
+            }
+        }
+        System.out.print("}");
+       */
+
     }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -8,7 +8,31 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
-        // add your tests here
+        int inputs[][] = {
+                {},
+                {3},
+                {3},
+                {1,3,3,6,1,8,0,10,22,4,3},
+                {1,2,3,4,5,6,7,8,9,2},
+                {4,6,8,2,10,11,12,13},
+                {10,9,8,7,6,5,4,3,2,1},
+        };
+        int answers[][] = {
+                {},
+                {3},
+                {3},
+                {0,1,1,3,3,3,4,6,8,10,22},
+                {1,2,2,3,4,5,6,7,8,9},
+                {2,4,6,8,10,11,12,13},
+                {1,2,3,4,5,6,7,8,9,10},
+        };
+
+        assertEquals(inputs.length, answers.length);
+        // pass x array from inputs[][] to bubbleSort, get output, compare output to x array in answers[][]
+        for (int i = 0; i < inputs.length; i++) {
+            Problem2.bubbleSort(inputs[i]);
+            assertArrayEquals(answers[i], inputs[i]);
+        }
+
     }
 }
